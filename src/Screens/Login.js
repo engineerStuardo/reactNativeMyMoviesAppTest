a7ee5abb67f6c1fb9b7bdcb570cfc96d6c3d162e
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-07-02 01:20:42 -0600

    adding icon, splash screen and animation to login

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -10,14 +10,17 @@ import axios from 'axios';
 import Toast from 'react-native-toast-message';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 
 import { baseURL } from '../../assets/common/baseUrl';
 
 const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export const Login = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const validation = () => {
     if (email === '' || password === '') {
@@ -27,6 +30,7 @@ export const Login = ({ navigation }) => {
         text1: 'Please enter credentials',
         text2: 'Please try again',
       });
+      setLoading(false);
       return '';
     }
   };
@@ -39,10 +43,12 @@ export const Login = ({ navigation }) => {
     try {
       const res = await axios.post(baseURL, body);
       if (res.status === 200) {
+        setLoading(false);
         AsyncStorage.setItem('token', res.data.token);
         navigation.navigate('Home');
       }
     } catch (error) {
+      setLoading(false);
       Toast.show({
         topOffset: 60,
         type: 'error',
@@ -53,43 +59,106 @@ export const Login = ({ navigation }) => {
   };
 
   const login = () => {
+    setLoading(true);
     if (validation() === '') return;
     apiRequest();
   };
 
+  useFocusEffect(
+    useCallback(() => {
+      setLoading(true);
+      AsyncStorage.removeItem('token')
+        .then(res => {
+          AsyncStorage.getItem('token')
+            .then(res => {
+              console.log(res);
+              setLoading(false);
+            })
+            .catch(error => console.log(error));
+        })
+        .catch(error => console.log(error));
+      // AsyncStorage.getItem('token')
+      //   .then(res => {
+      //     res ? navigation.navigate('Home') : setLoading(false);
+      //   })
+      //   .catch(error => console.log(error));
+    }, [])
+  );
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator
+          animating={true}
+          color={Colors.orange800}
+          size={'large'}
+        />
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, marginTop: 150, alignSelf: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // height: windowHeight / 3,
+        // backgroundColor: 'orange',
+        marginTop: windowHeight / 10,
+      }}
+    >
       <View
         style={{
-          width: windowWidth / 1.3,
-          backgroundColor: 'white',
-          padding: 40,
+          width: 150,
+          height: 150,
+          position: 'absolute',
+          top: 35,
+          left: 100,
         }}
       >
-        <Text style={{ fontSize: 20, marginBottom: 20 }}>Login</Text>
-        <TextInput
-          style={{ marginBottom: 25 }}
-          label='Email'
-          value={email}
-          onChangeText={text => setEmail(text)}
-          keyboardType={'email-address'}
+        <LottieView
+          key='animation'
+          autoPlay
+          loop
+          resizeMode='cover'
+          source={require('../../assets/movie.json')}
         />
-        <TextInput
-          style={{ marginBottom: 25 }}
-          label='Password'
-          value={password}
-          onChangeText={text => setPassword(text)}
-          secureTextEntry={true}
-        />
-        <View style={{ alignSelf: 'center' }}>
-          <Button
-            style={{ width: 150 }}
-            icon='login'
-            mode='contained'
-            onPress={login}
-          >
-            Login
-          </Button>
+      </View>
+      <View style={{ flex: 1, marginTop: 165, alignSelf: 'center' }}>
+        <View
+          style={{
+            width: windowWidth / 1.3,
+            backgroundColor: 'white',
+            padding: 40,
+          }}
+        >
+          {/* <Text style={{ fontSize: 20, marginBottom: 20 }}>Login</Text> */}
+          <TextInput
+            style={{ marginBottom: 25 }}
+            label='Email'
+            value={email}
+            onChangeText={text => setEmail(text)}
+            keyboardType={'email-address'}
+          />
+          <TextInput
+            style={{ marginBottom: 25 }}
+            label='Password'
+            value={password}
+            onChangeText={text => setPassword(text)}
+            secureTextEntry={true}
+            onSubmitEditing={login}
+          />
+          <View style={{ alignSelf: 'center' }}>
+            <Button
+              style={{ width: 150 }}
+              icon='login'
+              mode='contained'
+              onPress={login}
+            >
+              Login
+            </Button>
+          </View>
         </View>
       </View>
     </View>
